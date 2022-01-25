f5df4767705daf308c2e91817d8a0412a5372f79
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-01-25 11:27:41 -0700

    Base commit of OdinHose
    Signed-off-by: sbtoonz <67915879+sbtoonz@users.noreply.github.com>

---
+++ OdinHorse/OdinHorse.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ItemManager;
+using PieceManager;
+using CreatureManager;
+using ServerSync;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace OdinHorse
+{
+    [BepInPlugin(HGUIDLower, ModName, ModVersion)]
+    public partial class OdinHorse : BaseUnityPlugin
+    {
+
+        #region All Variables
+
+        public const string ModVersion = "1.0.0";
+        public const string ModName = "OdinMonstersReaper";
+        internal const string Author = "Raelaziel";
+        internal const string HGUID = Author + "." + "OdinMonstersReaper";
+        internal const string HGUIDLower = "Raelaziel.OdinMonstersReaper";
+        private const string HarmonyGUID = "Harmony." + Author + "." + ModName;
+        private static string ConfigFileName = HGUIDLower + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        public static string ConnectionError = "";
+        internal static Creature raeHorse;
+        
+        //logger
+        public static readonly ManualLogSource CreatureManagerModTemplateLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        //config
+        private readonly ConfigSync ConfigSync = new(ModName)
+        { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        //harmony
+        private readonly Harmony _harmony = new(HGUIDLower);
+
+        #endregion
+
+        private void Awake()
+        {
+            _serverConfigLocked = Config.Bind("General", "Force Server Config", true, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            #region Creature and Assets
+
+            raeHorse = new("odinhorse", "rae_OdinHorse")
+            {
+                Biome = Heightmap.Biome.Meadows,
+                SpawnChance = 30,
+                GroupSize = new Range(1, 2),
+                CheckSpawnInterval = 600,
+                SpecificSpawnTime = SpawnTime.Day,
+                RequiredWeather = new List<Weather> { Weather.ClearSkies },
+                Maximum = 1
+            };
+            raeHorse.Localize()
+                .English("Horse")
+                .German("Horse")
+                .French("Horse")
+                .Polish("Koń");
+            raeHorse.Drops["Coal"].Amount = new Range(1, 2);
+            raeHorse.Drops["Coal"].DropChance = 100f;
+            raeHorse.Drops["Flametal"].Amount = new Range(1, 1);
+            raeHorse.Drops["Flametal"].DropChance = 10f;
+            Patches.HorseObject = raeHorse.Prefab;
+            raeHorse.Prefab.AddComponent<BagOnEnable>();
+
+            Item raeHorseSaddle = new Item("odinhorse", "rae_SaddleHorse");
+            raeHorseSaddle.Name.English("Horse Saddl"); // You can use this to fix the display name in code
+            raeHorseSaddle.Description.English("Saddle for your horse");
+             raeHorseSaddle.Crafting.Add(CraftingTable.Workbench, 1);
+            raeHorseSaddle.MaximumRequiredStationLevel = 5; // Limits the crafting station level required to upgrade or repair the item to 5
+            raeHorseSaddle.RequiredItems.Add("Iron", 120);
+            raeHorseSaddle.RequiredItems.Add("WolfFang", 20);
+            raeHorseSaddle.RequiredItems.Add("Silver", 40);
+            raeHorseSaddle.CraftAmount = 1; // We really want to dual wield these
+
+            GameObject rae_Horse_stomp = ItemManager.PrefabManager.RegisterPrefab("odinhorse", "rae_Horse_stomp");
+            GameObject rae_OdinHorse_ragdoll = ItemManager.PrefabManager.RegisterPrefab("odinhorse", "rae_OdinHorse_ragdoll");
+
+            #endregion
+
+            // Check & Patch
+            SetupWatcher();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+        }
+
+        #region Configs
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                CreatureManagerModTemplateLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                CreatureManagerModTemplateLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                CreatureManagerModTemplateLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+        public static ConfigEntry<bool>?
+            _serverConfigLocked;
+
+        
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+
+        public class BagOnEnable : MonoBehaviour
+        {
+            private void Start()
+            {
+                var HumanoidAI = gameObject.GetComponent<Humanoid>();
+                if (HumanoidAI.IsTamed())
+                {
+                    var temptransform =
+                        OdinHorse.raeHorse.Prefab.transform.Find(
+                            "Visual/horse_BIP/horse Pelvis/horse Spine/horse Spine1/BagBag");
+                    if (temptransform)
+                    {
+                        temptransform.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    var temptransform =
+                        OdinHorse.raeHorse.Prefab.transform.Find(
+                            "Visual/horse_BIP/horse Pelvis/horse Spine/horse Spine1/BagBag");
+                    if (temptransform)
+                    {
+                        temptransform.gameObject.SetActive(false);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ OdinHorse/Patches.cs
@@ -0,0 +1,139 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+public class Patches
+{
+    internal static GameObject HorseObject;
+    private ItemDrop HorseDrop;
+    private static Container HorseContainer;
+    private static Container NormalBox;
+    private static Container tempBox;
+
+
+    [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.CopyOtherDB))]
+    public static class ObjectCopyPatch
+    {
+        public static void Prefix(ObjectDB __instance)
+        {
+            if (__instance.m_items.Count <= 0 || __instance.GetItemPrefab("Wood") == null) return;
+            NormalBox = ZNetScene.instance.GetPrefab("piece_chest_wood").gameObject.GetComponent<Container>();
+            if (NormalBox) AddContainer(HorseObject);
+        }
+    }
+
+    [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.Awake))]
+    public static class ObjectAwakePatch
+    {
+        public static void Prefix(ObjectDB __instance)
+        {
+            if (__instance.m_items.Count <= 0 || __instance.GetItemPrefab("Wood") == null) return;
+            NormalBox = ZNetScene.instance.GetPrefab("piece_chest_wood").gameObject.GetComponent<Container>();
+            if (NormalBox) AddContainer(HorseObject);
+        }
+    }
+
+    private static void AddContainer(GameObject attachObject)
+    {
+        HorseContainer = attachObject.AddComponent<Container>();
+        HorseContainer = new Container();
+        HorseContainer.m_height = 4;
+        HorseContainer.m_width = 6;
+        HorseContainer.m_bkg = NormalBox.m_bkg;
+        HorseContainer.m_inventory = new Inventory("HorseBox", HorseContainer.m_bkg, HorseContainer.m_height,
+            HorseContainer.m_width);
+        HorseContainer.m_rootObjectOverride = attachObject.GetComponent<ZNetView>();
+        HorseContainer.m_name = "SaddleBags";
+    }
+
+
+
+    [HarmonyPatch(typeof(Tameable), nameof(Tameable.Interact))]
+    public static class InteractPatch
+    {
+        public static bool Prefix(Tameable __instance)
+        {
+            if (__instance.gameObject.name.StartsWith("rae_OdinHorse"))
+            {
+                if (!__instance.gameObject.GetComponent<Humanoid>().IsTamed()) return true;
+                var container = __instance.gameObject.GetComponent<Container>();
+                if (Input.GetKey(KeyCode.P))
+                {
+                    container.Interact(Player.m_localPlayer, false, false);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Tameable), nameof(Tameable.Tame))]
+    public static class TameBagPatch
+    {
+        public static void Prefix(Tameable __instance)
+        {
+            if (!__instance.gameObject.name.StartsWith("rae_OdinHorse")) return;
+            var temptransform =
+                OdinHorse.OdinHorse.raeHorse.Prefab.transform.Find(
+                    "Visual/horse_BIP/horse Pelvis/horse Spine/horse Spine1/BagBag");
+            if (temptransform)
+            {
+                temptransform.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Tameable), nameof(Tameable.GetHoverText))]
+    public static class HoverTextPatch
+    {
+        public static void Postfix(ref string __result, Tameable __instance)
+        {
+            if (!__instance.gameObject.name.StartsWith("rae_OdinHorse")) return;
+            if (!__instance.gameObject.GetComponent<Humanoid>().IsTamed()) return;
+            __result += global::Localization.instance.Localize("\n<color=yellow>P & $KEY_Use  to open container</color>");
+        }
+    }
+
+    [HarmonyPatch(typeof(CharacterDrop), nameof(CharacterDrop.OnDeath))]
+    public static class DropHorseStuffPatch
+    {
+        public static void Prefix(CharacterDrop __instance)
+        {
+            if (!__instance.gameObject.name.StartsWith("rae_OdinHorse")) return;
+            if (Player.m_localPlayer == null) return;
+            var znvew = __instance.gameObject.GetComponent<ZNetView>();
+            if (!znvew.IsOwner()) return;
+            DropItemsOnDeath(__instance.gameObject.GetComponent<Container>());
+        }
+    }
+    private static void DropItemsOnDeath(Container box)
+    {
+        var position = new Vector3(Player.m_localPlayer.transform.position.x + 2, Player.m_localPlayer.transform.position.y, Player.m_localPlayer.transform.position.z);
+        var newbox = Object.Instantiate(Patches.NormalBox.gameObject, position,
+            Quaternion.identity, null);
+        var boxstone = ZNetScene.instance.GetPrefab("Player_tombstone");
+        var GraveStoneMesh = boxstone.transform.Find("Group8801").gameObject;
+        var GOCanvas = boxstone.transform.Find("Canvas").gameObject;
+
+        var container = newbox.GetComponent<Container>();
+        container.m_open = null;
+        container.m_closed = null;
+
+        var containerMesh = newbox.transform.Find("New/woodchest").gameObject;
+        var NewTransfomr = newbox.transform.Find("New");
+        newbox.transform.Find("New/woodchesttop_closed").gameObject.SetActive(false);
+        Object.DestroyImmediate(containerMesh);
+        var tempObj = Object.Instantiate(GraveStoneMesh, NewTransfomr, false);
+        var tempCanvas = Object.Instantiate(GOCanvas, newbox.transform, false);
+        tempCanvas.transform.Find("Text").gameObject.GetComponent<Text>().text = "SaddleBags";
+        foreach (var Idata in box.m_inventory.GetAllItems())
+        {
+            container.m_inventory.m_inventory.Add(Idata);
+        }
+
+        container.m_autoDestroyEmpty = true;
+        container.m_name = "SaddleBag";
+    }
+
+}
